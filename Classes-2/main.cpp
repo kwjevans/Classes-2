d48f48e5f85279457a6c39df03f8efb0e1c2f800
Kris Evans <kwjevans@gmail.com>
2012-08-04 23:43:38 +0100

    Initial Commit

---
+++ Classes-2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Classes-2
+//
+//  Created by Kris Evans on 04/08/2012.
+//  Copyright (c) 2012 Kris Evans. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
