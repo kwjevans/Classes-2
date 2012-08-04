f3a0b0505d39421a0884c11ab8ec0c0f4e176003
Kris Evans <kwjevans@gmail.com>
2012-08-05 00:49:01 +0100

    Mostly working, although answers not quite correct

--- Classes-2/main.cpp
+++ Classes-2/main.cpp
@@ -8,11 +8,63 @@
 
 #include <iostream>
 
+
+// Declaration of counter object
+int i;
+int currentNumber;
+int currentnumRooms;
+bool currenthasGarage;
+
+// Definition of apartment class
+
+class apartment {
+    
+    int number;
+    int numRooms;
+    
+    // bool hasGarage;
+
+public:
+    void Apartment(int number,int numRooms,bool hasGarage);
+    int aboutApartmentNumber(apartment currentApartment) const;
+    int aboutApartmentNumRooms(apartment currentApartment) const;
+};
+
+
+// Create an array of placeToLive of type apartment (our struct)
+apartment placeToLive[3];
+apartment thisApartment;
+
+// Constructor function for creating a new apartment structure
+void apartment::Apartment(int numberInit,int numRoomsInit,bool hasGarageInit){
+    number = numberInit;
+    numRooms = numRoomsInit;
+    //hasGarage = hasGarageInit;
+};
+
+inline int apartment::aboutApartmentNumber(apartment currentApartment) const {
+    return number;
+}
+
+
+inline int apartment::aboutApartmentNumRooms(apartment currentApartment) const {
+    return numRooms;
+}
+
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    for (i=1;i<4;i++){
+        placeToLive[i].Apartment(i*2, i, 0);
+    }
+    
+    for (i=1;i<4;i++){
+        currentNumber = thisApartment.aboutApartmentNumber(placeToLive[i]);
+        currentnumRooms = thisApartment.aboutApartmentNumRooms(placeToLive[i]);
+        
+        std::cout << i << ". Apartment number " << currentNumber << " has " << currentnumRooms << " rooms\n";
+    }
+    
     return 0;
 }
 
